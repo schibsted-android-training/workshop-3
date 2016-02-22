ac9c5d91633ab9c036bec70c787d8a4e573f7b30
Roger Martinez Teres <roger.martinez@infojobs.net>
2016-02-22 12:36:27 +0100

    getItemCount implementation

--- app/src/main/java/io/schibsted/workshop3/OfferAdapter.java
+++ app/src/main/java/io/schibsted/workshop3/OfferAdapter.java
@@ -39,7 +39,7 @@ public class OfferAdapter extends RecyclerView.Adapter<OfferAdapter.ViewHolder> 
 
     @Override
     public int getItemCount() {
-        return 0;
+        return offers.size();
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
