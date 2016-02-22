a851f5dab97a1e16bd8a0c1b7d75e167fb819e33
Roger Martinez Teres <roger.martinez@infojobs.net>
2016-02-22 12:28:32 +0100

    Create Recyclerview.Adapter

---
+++ app/src/main/java/io/schibsted/workshop3/OfferAdapter.java
@@ -0,0 +1,38 @@
+package io.schibsted.workshop3;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by roger.martinez on 22/2/16.
+ */
+public class OfferAdapter extends RecyclerView.Adapter<OfferAdapter.ViewHolder> {
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView titleTV;
+        TextView companyNameTV;
+        TextView cityTV;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            titleTV = ((TextView) itemView.findViewById(R.id.tv_title));
+            companyNameTV = ((TextView) itemView.findViewById(R.id.tv_company_name));
+            cityTV = ((TextView) itemView.findViewById(R.id.tv_city));
+        }
+    }
+}
