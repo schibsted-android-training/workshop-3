0aa680dbf77df13d8d1441bfc68b416621cb90ae
Roger Martinez Teres <roger.martinez@infojobs.net>
2016-02-22 12:33:27 +0100

    onCreateViewHolder implementation

--- app/src/main/java/io/schibsted/workshop3/OfferAdapter.java
+++ app/src/main/java/io/schibsted/workshop3/OfferAdapter.java
@@ -1,6 +1,8 @@
 package io.schibsted.workshop3;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -20,7 +22,11 @@ public class OfferAdapter extends RecyclerView.Adapter<OfferAdapter.ViewHolder> 
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View offerView = inflater.inflate(R.layout.item_offer, parent, false);
+        ViewHolder viewHolder = new ViewHolder(offerView);
+        return viewHolder;
     }
 
     @Override
